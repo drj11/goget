fbbe75f0b357348204ff577bf3868492f92bf886
David Jones <drj@pobox.com>
2015-11-02 21:01:00 +0000

    Write to output file, if checksum match

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"regexp"
 )
 
+var flagOut = flag.String("out", "./goget.out",
+	"pathname of output file")
 var flagSHA256 = flag.String("sha256", "",
 	"SHA-256 from the SHA-2 standard (as hexstring)")
 
@@ -23,6 +28,12 @@ func main() {
 		log.Fatalf("-sha256 \"%v\" should be a SHA-256 hexadecimal string", *flagSHA256)
 	}
 
+	outDir, outBase := path.Split(*flagOut)
+	out, err := ioutil.TempFile(outDir, outBase)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	resp, err := http.Get("http://golang.org/")
 	if err != nil {
 		log.Fatal(err)
@@ -33,14 +44,29 @@ func main() {
 	// that tee-ing to the hash "never returns an error".
 	body := io.TeeReader(resp.Body, h)
 
-	_, err = io.Copy(os.Stdout, body)
+	_, err = io.Copy(out, body)
 	if err != nil {
 		log.Fatal(err)
 	}
+	outPath := out.Name()
+	out.Close()
 
-	b := h.Sum([]byte{})
-	for i := range b {
-		fmt.Fprintf(os.Stderr, "%02x", b[i])
+	buf := bytes.NewBuffer(nil)
+	bs := h.Sum([]byte{})
+	for i := range bs {
+		fmt.Fprintf(buf, "%02x", bs[i])
 	}
-	fmt.Fprint(os.Stderr, "\n")
+	gotChecksum := buf.String()
+
+	if gotChecksum != *flagSHA256 {
+		_ = os.Remove(outPath)
+		fmt.Fprint(os.Stderr,
+			"Expected checksum ", *flagSHA256,
+			"\nGot checksum ", gotChecksum, "\n")
+		os.Exit(1)
+	}
+	err = os.Rename(outPath, *flagOut)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
